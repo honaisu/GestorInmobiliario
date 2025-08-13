4e6d4fd0ea7254a1b62d0b5b0d91a34d2993efe9
honaisu <iruiz.aran@gmail.com>
2025-08-13 15:51:40 -0400

    Hello World

---
+++ gestionInmobiliario/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/** Proyecto SIA
+ * @author honaisu (Ignacio Ruiz)
+ * @author 
+ * @author 
+ */
+
+public class Main {
+    public static void main(String[] args) {
+	System.out.println("Hola Mundo!");
+    }
+}
